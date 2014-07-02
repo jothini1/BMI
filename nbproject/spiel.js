ef160173e528d3eced31de2e951c22a2278c49e8
srithj <srithj@IR1-PC-19-W7.schule.local>
2014-07-02 17:28:30 +0200

    Amfamgsseite erstellt

--- nbproject/spiel.js
+++ nbproject/spiel.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function(){
 $("body").html("Körpermasse in kg:  <input id='masse' type='text' size='10'></input><br>");   
-         $("body").append("Größe in m: <input id='groesse' type='text' size='10'></input><br>");
+         $("body").append("Größe in cm: <input id='groesse' type='text' size='10'></input><br>");
          $("body").append("<input id='berechnen' type='button' value='Berechnen'></input>");
          
          $("#berechnen").click(function(){
             var masse=parseInt($("#masse").val()); 
-            var groesse=parseDouble($("#groesse").val());
+            var groesse=parseInt($("#groesse").val());
            
-            var bmi=masse/(groesse*groesse);
+            var bmi=parseInt(masse/((groesse/100)*(groesse/100)));
             
             $("body").append("Der BMI beträgt "+bmi+".");
             
